2d5a2633d449e126a2e1111276b5b1938f06ceaa
CaCao64 <121355439+CaCao64@users.noreply.github.com>
2025-04-11 19:20:25 +0200

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+import time
+import requests
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is online!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
